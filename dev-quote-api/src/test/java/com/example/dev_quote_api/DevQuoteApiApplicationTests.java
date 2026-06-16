ff9685fb9fe3ccf42b44160f098d53367414b8b1
Abdallah Mohamed <amahaaq@gmail.com>
2026-06-16 21:36:36 +0300

    Initial commit: Spring Boot skeleton

---
+++ dev-quote-api/src/test/java/com/example/dev_quote_api/DevQuoteApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dev_quote_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevQuoteApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
